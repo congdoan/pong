72a5aecc9778cfa69bb595151af90a578f4cf94e
Cong Doan <cong.doan@gmail.com>
2019-07-11 22:44:52 +0700

    Starting code for pong-7.

---
+++ 6/pong-7/Paddle.lua
@@ -0,0 +1,34 @@
+--[[
+    GD50 2018
+    Pong Remake
+
+    -- Paddle Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Represents a paddle that can move up and down. Used in the main
+    program to deflect the ball back toward the opponent.
+]]
+
+Paddle = Class{}
+
+function Paddle:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.dy = 0
+end
+
+function Paddle:update(dt)
+    if self.dy < 0 then
+        self.y = math.max(0, self.y + self.dy * dt)
+    else
+        self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+    end
+end
+
+function Paddle:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
